b1b97f97985868728030c3fe03db58910e710586
Rodney <rod03072003@gmail.com>
2023-04-05 01:32:02 +0200

    Charge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { WikiService } from './wiki.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wiki-app';
+  searchTerm: any;
+  results: any = [];
+
+  totalResults: any;
+  page: number = 1;
+
+  constructor(private wiki: WikiService) {}
+
+  onSubmit() {
+    this.wiki.search(this.searchTerm).subscribe((res: any) => {
+      this.results = res.query.search;
+
+      this.totalResults = res.query.search.length;
+      console.log(this.results);
+    });
+  }
 }
